a5a20320b2dafe592d210fc9dc23ed8d338cf96c
creative280 <alejandro.taborda280@gmail.com>
2023-10-08 21:44:02 -0500

    Actualizacion del codigo al 60%

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/contactsdb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = '_5#y2L"F4Q8z15467*/'
 
-db = SQLAlchemy(app)  # Crear y registrar la instancia de SQLAlchemy en la aplicación
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root@localhost/contactsdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 from routes.contacts import contacts
 app.register_blueprint(contacts)
--- routes/contacts.py
+++ routes/contacts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.contact import Contact
 from app import db
 
@@ -7,34 +7,56 @@ contacts = Blueprint("contacts", __name__)
 
 
 @contacts.route("/")
-def home():
-    contacts = Contact.query.all() 
+def index():
+    contacts = Contact.query.all()
     return render_template("index.html", contacts=contacts)
 
 
-@contacts.route("/new", methods=['POST','GET'])
+@contacts.route("/new", methods=["POST", "GET"])
 def addContact():
-    fullname = request.form['fullname']
-    email = request.form['email']
-    phone = request.form['phone']
-    role = request.form['role']
+    fullname = request.form["fullname"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    role = request.form["role"]
 
     new_contact = Contact(fullname, email, phone, role)
 
     db.session.add(new_contact)
     db.session.commit()
 
-    return redirect('/')
+    flash("Contacto Añadido Correctamente")
+
+    return redirect(url_for("contacts.index"))
 
 
-@contacts.route("/update")
-def update():
-    return "Update Contact"
+@contacts.route("/update/<id>", methods=["POST", "GET"])
+def update(id):
+    contact = Contact.query.get(id)
+    if request.method == "POST":
+        contact.fullname = request.form["fullname"]
+        contact.email = request.form["email"]
+        contact.phone = request.form["phone"]
+        contact.role = request.form["role"]
+
+        db.session.commit()
+
+        flash("Contacto Actualizado Correctamente")
+
+        return redirect(url_for("contacts.index"))
+
+    return render_template("update.html", contact=contact)
 
 
-@contacts.route("/delete")
-def delete():
-    return "Delete Contact"
+@contacts.route("/delete/<id>")
+def delete(id):
+    contact = Contact.query.get(id)
+
+    db.session.delete(contact)
+    db.session.commit()
+
+    flash("Contacto Eliminado Correctamente")
+
+    return redirect(url_for("contacts.index"))
 
 
 @contacts.route("/about")
